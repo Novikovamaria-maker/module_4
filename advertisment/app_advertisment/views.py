82c327ed58f98e9b8c87d00f4948effd71ad4852
novikovamaria <marianovikova233@mail.ru>
2023-07-19 11:43:59 +0300

    урок 5

--- advertisment/app_advertisment/views.py
+++ advertisment/app_advertisment/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def  index(request):
-    return HttpResponse('Это мой первый сайт!!!')
+    return render(request, 'index.html')
 
+def  top_sellers(request):
+    return render(request, 'top-sellers.html')
