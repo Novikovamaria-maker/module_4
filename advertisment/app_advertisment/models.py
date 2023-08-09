4ec5739808b714058c8c221951fbbb9b877d6037
novikovamaria <marianovikova233@mail.ru>
2023-08-09 20:26:20 +0300

    lesson 8

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -1,16 +1,37 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
-# Create your models here.
+
 class Advertisement(models.Model):
-    title = models.CharField('Заголовок', max_length=120)
+    title = models.CharField('Заголовок', max_length=128)
     description = models.TextField('Описание')
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
-    auction = models.BooleanField('Торг', help_text='Ответьте, если торг уместен')
+    auction = models.BooleanField('Торг', help_text='Отьметьте, если торг уместен')
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
     
+    @admin.display(description='Дата изменения')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', updated_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
     def __str__(self):
-        return f"Advertisment(id={self.id}, title={self.title}, price={self.price})"
-    
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
     class Meta:
-        db_table="advertisements"
+        db_table = "advertisements"
