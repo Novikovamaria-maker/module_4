de90e0f16b6ca86790bfc3cee7a039b39aa98d39
novikovamaria <marianovikova233@mail.ru>
2023-08-17 15:48:16 +0300

    ls 9

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.utils.safestring import mark_safe
 
-
+User = get_user_model()
 class Advertisement(models.Model):
     title = models.CharField('Заголовок', max_length=128)
     description = models.TextField('Описание')
@@ -11,6 +13,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Отьметьте, если торг уместен')
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('Изображение', upload_to='advertisements/')
 
     @admin.display(description='Дата создания')
     def created_date(self):
@@ -29,7 +33,14 @@ class Advertisement(models.Model):
                 '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', updated_time
             )
         return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
-
+    
+    @admin.display(description='Изображение')
+    def image_img(self):
+        if self.image:
+            return format_html(
+                '<img scr="{url}" style="max-width: 80px; max-height: 80px:"', url=self.image.url
+            )
+        
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
 
