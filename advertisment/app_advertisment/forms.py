fd3ff72327cdc033810a8cd3eb4f6cc78817ec0e
novikovamaria <marianovikova233@mail.ru>
2023-08-24 15:50:28 +0300

    10-ls

---
+++ advertisment/app_advertisment/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Advertisement
+
+
+class AdvertisementForm(forms.ModelForm):
+    class Meta:
+        model = Advertisement
+        fields = ("title", "description", "price", "auction", "image")
+        widjets = {
+    'title': forms.TextInput(attrs={'class': 'form-control form-control-lg'}),
+    'description': forms.Textarea(attrs={'class': 'form-control form-control-lg'}),
+    'price': forms.NumberInput(attrs={'class': 'form-control form-control-lg'}),
+    'auction': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+    'image': forms.FileInput(attrs={'class': 'form-control form-control-lg'})
+        }
+        
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.startwith('?'):
+            raise ValidationError('Заголовок не может начинаться с вопросительного знакаю')
+        return title
